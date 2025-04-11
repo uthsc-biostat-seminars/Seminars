3fc2e9a001eaa3e7b70879239eb654b939aba0f9
chenhz1223 <czhao1223@gmail.com>
2025-04-11 18:19:37 -0500

    update data transfer

--- scripts/update_seminars.py
+++ scripts/update_seminars.py
@@ -1,1 +1,25 @@
 # script to update seminars TSV
+import pandas as pd
+from datetime import datetime
+
+past = pd.read_csv("../Seminars/past_seminars.tsv", sep="\t")
+upcoming = pd.read_csv("../Seminars/upcoming_seminars.tsv", sep="\t")
+
+def is_future_date(date_str):
+    date_obj = datetime.strptime(date_str, "%Y-%m-%d")
+    current_date = datetime.now()
+    return date_obj > current_date
+
+for i in range(len(upcoming)):
+    if is_future_date(upcoming.iloc[i]["Date"].replace('"', "")):
+        print(upcoming.iloc[i]["Date"].replace('"', ""))
+        upcoming.iloc[i]["Date"] = upcoming.iloc[i]["Date"].replace('"', "")
+    else:
+        print("past seminar")
+        past = pd.concat([past, upcoming.iloc[[i]]], ignore_index=True)
+        upcoming.drop(i, inplace=True)
+
+past.to_csv("../Seminars/past_seminars.tsv", sep="\t", index=False)
+upcoming.to_csv("../Seminars/upcoming_seminars.tsv", sep="\t", index=False)
+print("Updated past and upcoming seminars TSV files.")
+
