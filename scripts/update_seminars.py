68de74e7037553e4bea7336e8482713a146d2345
chenhz1223 <92123913+chenhz1223@users.noreply.github.com>
2025-04-16 00:26:07 -0500

    fix an error in script

--- scripts/update_seminars.py
+++ scripts/update_seminars.py
@@ -11,14 +11,17 @@ def is_future_date(date_str):
     return date_obj > current_date
 
 for i in range(len(upcoming)):
-    if is_future_date(upcoming.iloc[i]["Date"].replace('"', "")):
+
+    date = upcoming.iloc[i]["Date"].replace('"', "")
+    if is_future_date(date):
         continue
     else:
         print("past seminar")
         past = pd.concat([past, upcoming.iloc[[i]]], ignore_index=True)
-        upcoming.drop(i, inplace=True)
+        upcoming_new = upcoming.drop(i, inplace=False)
 
 past.to_csv("../Seminars/past_seminars.tsv", sep="\t", index=False)
-upcoming.to_csv("../Seminars/upcoming_seminars.tsv", sep="\t", index=False)
+upcoming_new.to_csv("../Seminars/upcoming_seminars.tsv", sep="\t", index=False)
+
 print("Updated past and upcoming seminars TSV files.")
 
